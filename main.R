361998aebc339582cfb77c283230be4d79d59b5a
baidurja <39422735+baidurja@users.noreply.github.com>
2018-10-17 19:09:33 -0500

    Add files via upload

--- main.R
+++ main.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(jsonlite)
+library(data.table)
 
 theData = read_csv('./data/train.csv')
 
@@ -7,9 +8,19 @@ theData$channelGrouping = as.factor( theData$channelGrouping )
 theData$fullVisitorId = as.factor( theData$fullVisitorId )
 theData$socialEngagementType = as.factor( theData$socialEngagementType )
 
-theData = unpackJSONCol( theData, theData$totals )
-theData$totals = NULL
-theData = unpackJSONCol( theData, theData$device )
+totals_col = json_clean( theData$totals )
+device_col = json_clean( theData$device )
+geonet_col = json_clean( theData$geoNetwork )
+traffsrc_col = json_clean( theData$trafficSource )
+
+# k = theData$totals
+# d = data.frame( jsoncol = k, stringsAsFactors = FALSE )
+# 
+# theData = unpackJSONCol( theData, theData$totals )
+# theData$totals = NULL
+# theData = unpackJSONCol( theData, theData$device )
+
+gc()
 
 ggplot( theData, aes( channelGrouping ) ) + geom_bar()
 
@@ -19,7 +30,13 @@ ggplot( theData, aes( channelGrouping ) ) + geom_bar()
 
 
 
-
+json_clean =  function(column){
+  col <- column %>% gsub('\"\"','\"',.) #turn into json format
+  rs <- lapply(col, function(x) jsonlite::fromJSON(x)) # read json into data format
+  ff <- rbindlist(rs, fill=TRUE) # turn list into dataframe
+  gc()
+  return(ff)
+}
 
 
 unpackJSONCol = function( theDF, theDFCol )
